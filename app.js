801f96573a245b0abf45904d0944cacd32399ed2
gupta-yash4222 <guptayash4222@gmail.com>
2021-07-13 15:20:59 +0530

    Heroku getting started

--- app.js
+++ app.js
@@ -75,4 +75,6 @@ app.get('/:roomID', (req, res) => {
 
 io.on('connection', stream.stream)
 
-server.listen(3000, () => {console.log("Listening to port no. 3000 .....")})
+const port = process.env.PORT || 3000
+
+server.listen(port, () => {console.log(`Listening to port no. ${port} .....`)})
