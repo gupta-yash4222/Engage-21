918d16dec078106b3f23ad68588d54e4acb18a78
gupta-yash4222 <guptayash4222@gmail.com>
2021-07-13 18:06:41 +0530

    Heroku hosting

--- app.js
+++ app.js
@@ -6,20 +6,25 @@ const express = require('express');
 const app = express();
 const http = require('http')
 server = http.Server(app)
-const io = require('socket.io')(server)
+const io = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+    }
+})
 const { v4: uuidV4 } = require('uuid')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const stream = require('./stream.js')
 const ExpressPeerServer = require('peer').ExpressPeerServer
 
-const PeerServer = require('peer').PeerServer;
-const peer_server = PeerServer({port: 3001, path: '/'})
+//const PeerServer = require('peer').PeerServer;
+//const peer_server = PeerServer({port: 3001, path: '/'})
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.use('/public', express.static('./public'))
 app.use('/peer', ExpressPeerServer(server, {
+    debug: true, 
     allow_discovery: true
 }))
 app.use(express.urlencoded({ extended: false }))
@@ -45,27 +50,6 @@ app.get('/', (req, res) => {
     //res.redirect(`/${uuidV4()}`)
     res.render(__dirname + '/views/room.ejs', { roomID: uuidV4()})
 })
-
-/*
-
-app.get('/login', (req, res) => {
-    res.render(__dirname + '/views/login.ejs')
-})
-
-app.post('/login', (req, res) => {
-    console.log(req.body)
-})
-
-app.get('/signup', (req, res) => {
-    res.render(__dirname + '/views/signup.ejs')
-})
-
-app.post('/signup', async (req, res) => {
-
-})
-
-*/
-
 
 const userRouter = require('./routes/user')
 app.use('/api', userRouter)
--- public/js/script.js
+++ public/js/script.js
@@ -30,8 +30,9 @@ window.addEventListener('load', () => {
         //const socket = io('/')
         const videoGrid = document.getElementById('video-grid')
         const peer = new Peer(undefined, {
+            path: '/peerjs',
             host: '/',
-            port: '3001'
+            port: '443'
         })
         
         const myVideo = document.createElement('video')
